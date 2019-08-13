9a202b12e1e9d857c9cf24b25951c2abf95f2c25
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2019-08-13 18:56:09 -0300

    commit inicial

---
+++ InfraIdentity/UsuarioLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfraIdentity
+{
+    public class UsuarioLogin
+    {
+        //esta é a chave 
+        public string Apelido { get; set; }
+        public string Nome { get; set; }
+    }
+}
