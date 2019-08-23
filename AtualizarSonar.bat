c132e5739555dabe0a757f55510d2df808224ed7
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2019-08-23 20:13:57 -0300

    configuração do sonarqube

---
+++ AtualizarSonar.bat
@@ -0,0 +1,5 @@
+rem usando caminhos fixos
+
+"C:\utils\sonar-scanner-4.0.0.1744-windows\bin\sonar-scanner.bat" -D"sonar.projectKey=ArClube.PrepedidoSpa" -D"sonar.sources=." -D"sonar.host.url=http://sonar.its.teste:9000" -D"sonar.login=1fcadb0094a0e4cd73d7d93461466dbb24f4e086"
+
+pause
