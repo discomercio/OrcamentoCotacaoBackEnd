7e8cd63079b5f2a187321ab6da6d061753e2d40e
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2019-09-26 12:20:31 -0300

    lista de produtos carregando mais rápido

--- src/app/prepedido/novo-prepedido/selec-prod-dialog/selec-prod-dialog.component.ts
+++ src/app/prepedido/novo-prepedido/selec-prod-dialog/selec-prod-dialog.component.ts
@@ -21,45 +21,75 @@ export class SelecProdDialogComponent extends TelaDesktopBaseComponent implement
     telaDesktopService: TelaDesktopService,
     public readonly alertaService: AlertaService) {
     super(telaDesktopService);
+
+    //no desktop deixamos um limite inicial maior
+    this.limiteInicial = 20;
+    if (this.telaDesktop) {
+      this.limiteInicial = 80;
+    }
+    this.limiteTela = this.limiteInicial;
+
   }
 
   //#region carga
+  public prodsArray: ProdutoTela[] = new Array();
   //dados vazios, para mostrar enquanto constroi a lista
-  public prods: ProdutoTela[] = new Array();
-  //se estmoas carregando ainda
-  public carregando = true;
-  private maisDadosProd() {
-    //transfere alguns dados por vez
-    for (let copiar = 0; copiar < 30; copiar++) {
+  public prodsTela: ProdutoTela[] = new Array();
+
+  /*
+  a tela com 600 produtos ficava muito lenta
+  então temos o seguinte fucnionamento: ao carregar, mostra os primerios X registros
+  depois, se clicar no botão para mostrar tudo, aí carrega todos, e demora o que demorar...
+  */
+
+  //maximo que mostramos na tela
+  public limiteMaximo = 1000 * 1000; //1 mega registros...
+  //quantos mostramos na tela inicialmente
+  public limiteInicial = 50;
+  public limiteTela = this.limiteInicial;
+
+  //se estamos carregando a lista completa de produtos
+  //mas ainda não terminou de mostrar
+  //aí o botão muda, ao inves de ser Mostrar todo, fica Carregando
+  public limiteMudando = false;
+
+  public limiteZerar() {
+    //mostramos sem limites
+    this.limiteTela = this.limiteMaximo;
+    this.limiteMudando = true;
+    setTimeout(() => {
+      this.atualizarProdsTela();
+      this.limiteMudando = false;
+    }, 0);
+  }
+
+  private transferirDadosArray() {
+    //sem limite!
+    const limite = this.limiteMaximo;
+    for (let copiar = 0; copiar < limite; copiar++) {
       //acabou?
-      if (!(this.prods.length < this.selecProdInfoPassado.produtoComboDto.ProdutoDto.length))
+      if (!(this.prodsArray.length < this.selecProdInfoPassado.produtoComboDto.ProdutoDto.length))
         break;
       //colocamos mais um
-      this.prods.push(new ProdutoTela(this.selecProdInfoPassado.produtoComboDto.ProdutoDto[this.prods.length], this.selecProdInfoPassado.produtoComboDto.ProdutoCompostoDto));
+      this.prodsArray.push(new ProdutoTela(this.selecProdInfoPassado.produtoComboDto.ProdutoDto[this.prodsArray.length], this.selecProdInfoPassado.produtoComboDto.ProdutoCompostoDto));
     }
 
-    ProdutoTela.AtualizarVisiveis(this.prods, this.digitado);
-    //precisa mais vezes para transferir?
-    if (this.prods.length < this.selecProdInfoPassado.produtoComboDto.ProdutoDto.length) {
-      //proximo bloco
-      setTimeout(() => this.maisDadosProd(), 1);
-    }
-    else {
-      //já acabou de carregar
-      this.carregando = false;
-    }
+    ProdutoTela.AtualizarVisiveis(this.prodsArray, this.digitado);
+    this.atualizarProdsTela();
   }
+
+  public atualizarProdsTela() {
+    this.prodsTela = this.prodsArray.filter(el => el.visivel).slice(0, this.limiteTela);
+  }
+
   ngOnInit() {
-    //transfere só os X primeiros, para a tela carregar mais rápido
-    //sem isto, ela já mostra assim que carregar, mas fica travada enquanto é montada. 
-    this.carregando = true;
     this.produto = ProdutoTela.FabrProd(this.selecProdInfoPassado.Fabricante, this.selecProdInfoPassado.Produto);
     this.qdte = this.selecProdInfoPassado.Qte;
     if (this.selecProdInfoPassado.Produto) {
       this.digitado = this.selecProdInfoPassado.Produto;
     }
 
-    this.maisDadosProd();
+    this.transferirDadosArray();
   }
   //#endregion
 
@@ -79,7 +109,8 @@ export class SelecProdDialogComponent extends TelaDesktopBaseComponent implement
   digitado = "";
   digitouProd(e: Event) {
     this.digitado = ((e.target) as HTMLInputElement).value;
-    ProdutoTela.AtualizarVisiveis(this.prods, this.digitado);
+    ProdutoTela.AtualizarVisiveis(this.prodsArray, this.digitado);
+    this.atualizarProdsTela();
   }
 
   onNoClick(): void {
