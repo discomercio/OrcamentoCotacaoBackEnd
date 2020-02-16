dd6431e1db873733c3733ab3061099cd6b91e21a
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2020-02-16 15:56:03 -0300

    datatables com amterialize

---
+++ Loja.Modelo/TusuarioXLoja.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Loja.Modelos
+{
+    [Table("t_USUARIO_X_LOJA")]
+    public class TusuarioXLoja
+    {
+    }
+}
