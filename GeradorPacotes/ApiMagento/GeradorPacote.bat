c457cb3733575fb10481d2a8a680c26ad45cc619
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2020-07-30 11:40:40 -0300

    processo de compilação apimagento

--- GeradorPacotes/ApiMagento/GeradorPacote.bat
+++ GeradorPacotes/ApiMagento/GeradorPacote.bat
@@ -1,5 +1,4 @@
-rem compilar o porjeto da API
-rem para a compilacao total, usar o PrepedidoSpa\CompilarPrepedidoSpaApi.bat, no projeto do SPA
+rem compilar o projeto da ApiMagento
 set aux_msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 
 IF EXIST %aux_msbuild% goto msbuild_encontrado
@@ -15,8 +14,10 @@ RD PacoteApiMagento /s /q >nul 2> nul
 
 %aux_msbuild% ../../ApiMagento/ApiMagento/ApiMagento/ApiMagento.csproj -t:Rebuild -p:DeployOnBuild=true -p:PublishProfile=CompilarApiMagento /p:Configuration=Release
 
-echo %date% %time% >> "PacoteApiMagento/DataCompilacao.txt"
-
+echo %date% %time% >> "PacoteApiMagento/publish/DataCompilacao.txt"
+del PacoteApiMagento\publish\appsettings.json
+del PacoteApiMagento\publish\appsettings.Development.json
+del PacoteApiMagento\publish\nlog.config
 
 echo pacote gerado em PacoteApiMagento
 IF "%~1" NEQ "sem_pausa" pause
