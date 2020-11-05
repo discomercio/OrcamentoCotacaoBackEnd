e3685e13d17be61122944e0cb5f8f8ab1a3de7b2
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2020-11-05 11:44:29 -0300

    Movendo script para arquivo Cabecalho.ts

---
+++ Loja/Loja.UI/Views/Shared/Cabecalho.ts
@@ -0,0 +1,30 @@
+﻿
+declare var usuarioLogado: any;
+
+$(function () {
+    if (usuarioLogado != "" && usuarioLogado != undefined) {
+        var split = usuarioLogado.split(' ');
+        if (split.length > 1) {
+            debugger;
+        }
+        else {
+            var letra = usuarioLogado.substring(0, 2);
+            setRandomColor();
+            $("#avatar").text(letra.toUpperCase());
+            $("#menu-usuario").attr("data-original-title", usuarioLogado);
+        }
+
+    }
+});
+
+function setRandomColor() {
+    $("#avatar").css("background-color", getRandomColor());
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
