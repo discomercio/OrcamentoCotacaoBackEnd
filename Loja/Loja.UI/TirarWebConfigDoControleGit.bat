cf77c52fdcc42156b369638b487f7735ce1090df
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2020-11-11 12:12:42 -0300

    BAT para tirar web.config do controle do git

---
+++ Loja/Loja.UI/TirarWebConfigDoControleGit.bat
@@ -0,0 +1,6 @@
+rem o web.config vira e mexe entra como arquivo modificado
+rem este comando é para que alterações no web.config sejam ignoradas
+rem precisa ser executado quando se faz um novo clone do código
+git update-index --assume-unchanged web.config
+pause
+
