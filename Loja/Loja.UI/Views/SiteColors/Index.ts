898cd1ad8c226f257a99578d114e0466a1afa73f
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2020-12-15 17:52:43 -0300

    Alteração de link na tela de pedido.asp e orcamento.asp. Tratamento para componetes da tela do asp Alteração na numeração de páginas do SiteColors Alteração no link do botão voltar da tela DadosCliente.cshtml

--- Loja/Loja.UI/Views/SiteColors/Index.ts
+++ Loja/Loja.UI/Views/SiteColors/Index.ts
@@ -1,14 +1,6 @@
 ﻿
 class SiteColorsIndex {
     constructor() {
-
-        /*
-         * fazer uma função para trocar o href que contém resumo.asp para home mvc ou history.back()
-         * adicionar e incluir :
-         *      href => passar a url e incluir target="_top"
-         *      window.parent.location.href= "http://www.google.com";
-         */
-
         $(document).ready(() => {
 
             //carregamos a página e tiramos o estilo de carregamento quando terminar
@@ -17,28 +9,13 @@ class SiteColorsIndex {
             this.iframeJquery.parent().addClass("carregando");
             this.iframe.contentDocument.getElementsByTagName('body')[0].style.cursor = "wait";
 
-
-
             this.iframe.onload = () => {
 
-                if (this.iframe.contentDocument.location.href.indexOf("AjaxCepPesqPopup.asp") != -1) {
-                    if (this.iframe.contentDocument.getElementById("bFechar").textContent == 'Fechar')
-                        this.iframe.contentDocument.getElementById("bFechar").textContent = 'Voltar';
-                    this.iframe.contentDocument.getElementById("bFechar").setAttribute('onclick', 'javascript:history.back();');
-                }
+                this.VerificarPaginaASP();
+
                 this.iframe.contentDocument.getElementsByTagName('html')[0].style.overflowX = "hidden";
                 //estamos tratando os link's que direcionam para resumo.asp
                 this.AlterarHrefPagina();
-
-                /* não podemos ir para a tela principal do asp.
-                 * mesmo verificando todos os links das páginas asp vamos manter este bloco.
-                 * sabendo que o asp pode redirecionar de forma automática para página principal do asp,
-                 * criamos esse bloco para caso ocorra o redirecionamento para resumo.asp, iremos redirecionar 
-                 * para a Home do mvc
-                 */
-                if (this.iframe.contentDocument.location.href.indexOf("resumo.asp") != -1) {
-                    document.location.href = this.urlBase;
-                }
 
                 this.removerElementosTela();
                 this.iframeJquery.parent().removeClass("carregando");
@@ -54,26 +31,88 @@ class SiteColorsIndex {
         });
     }
 
+    private VerificarPaginaASP() {
+        /* não podemos ir para a tela principal do asp.
+         * mesmo verificando todos os links das páginas asp vamos manter este bloco.
+         * sabendo que o asp pode redirecionar de forma automática para página principal do asp,
+         * criamos esse bloco para caso ocorra o redirecionamento para resumo.asp, iremos redirecionar 
+         * para a Home do mvc
+         */
+        if (this.iframe.contentDocument.location.href.toLowerCase().indexOf("resumo.asp") != -1) {
+            document.location.href = this.urlBase;
+        }
+
+        //Essa tela não esta tratando a caixa com msg no topo
+        if (this.iframe.contentDocument.location.href.toLowerCase().indexOf("relvendasporboletoexec.asp") != -1) {
+            this.iframe.contentDocument.styleSheets[1].removeRule(2)
+        }
+
+        //vamos acertar o layout de algumas página que não estão sendo tratadas
+        if (this.iframe.contentDocument.location.href.toLowerCase().indexOf("senha.asp") != -1 ||
+            this.iframe.contentDocument.location.href.toLowerCase().indexOf("menufuncoesadministrativas.asp") != -1 ||
+            this.iframe.contentDocument.location.href.toLowerCase().indexOf("menuorcamentistaeindicador.asp") != -1) {
+            //vamos tratar
+            $(this.iframe.contentDocument).children().find(".PEDIDO").closest("table").hide();
+        }
+        //vamos mudar o direcionamento dos links 
+        if (this.iframe.contentDocument.location.href.toLowerCase().indexOf("pedido.asp") != -1 ||
+            this.iframe.contentDocument.location.href.toLowerCase().indexOf("orcamento.asp") != -1) {
+            this.AlterarHrefCliente();
+        }
+
+        if (this.iframe.contentDocument.location.href.toLowerCase().indexOf("ajaxceppesqpopup.asp") != -1) {
+            if (this.iframe.contentDocument.getElementById("bFechar").textContent == 'Fechar')
+                this.iframe.contentDocument.getElementById("bFechar").textContent = 'Voltar';
+            this.iframe.contentDocument.getElementById("bFechar").setAttribute('onclick', 'javascript:history.back();');
+        }
+    }
+
     private AlterarHrefPagina() {
         let lstLink: JQuery<HTMLAnchorElement>;
         lstLink = $(this.iframe.contentDocument).find("a");
         for (let i = 0; i < lstLink.length; i++) {
             let link = lstLink[i];
 
-            if (link.href.indexOf("resumo.asp") != -1) {
+            if (link.href.toLowerCase().indexOf("resumo.asp") != -1) {
                 lstLink[i].setAttribute("target", "_parent");
                 lstLink[i].setAttribute("href", this.urlBase);
             }
         }
     }
 
-    private AlterarHrefBtnVoltar() {
+    private AlterarHrefCliente() {
 
-        let href = $(this.iframe.contentDocument).find("#bVOLTAR").attr("href");
-        if (href.indexOf("resumo.asp") != -1) {
-            $(this.iframe.contentDocument).find("#bVOLTAR").attr('target', "_parent");
-            $(this.iframe.contentDocument).find("#bVOLTAR").attr('href', this.urlBase);
+        let urlCliente: string = this.urlBase + "/Cliente/ValidarCliente?cpf_cnpj=";
+        let lstLink: JQuery<HTMLAnchorElement>;
+        lstLink = $(this.iframe.contentDocument).find("[href='javascript:fCLIEdita();']") as JQuery<HTMLAnchorElement>;
+        let urlBase = this.urlBase;
+        if (lstLink != undefined) {
+            let cpf_cnpj: string = lstLink[0].children[0].textContent.trim();
+            for (let i = 0; i < lstLink.length; i++) {
+                lstLink[i].href = "#!";
+                $(lstLink[i]).on("click", function () {
+                    SiteColorsIndex.ValidarCliente(cpf_cnpj, urlBase);
+                });
+            }
         }
+    }
+
+    public static ValidarCliente(cpf_cnpj: string, urlBase: string): void {
+        $.ajax({
+            url: urlBase + "/Cliente/ValidarCliente",
+            type: "GET",
+            data: { cpf_cnpj: cpf_cnpj },
+            dataType: "json",
+            cache: false,
+            success: function (data) {
+                if (data != undefined) {
+                    document.location.href = urlBase + "/Cliente/BuscarCliente?cpf_cnpj=" + cpf_cnpj + "&&novoCliente=" + data;
+                }
+            },
+            error: function (data) {
+                swal("Erro", "Falha ai validar cliente!");
+            }
+        });
     }
 
     private removerElementosTela() {
@@ -88,19 +127,21 @@ class SiteColorsIndex {
 
     private removerLinhaCopyright() {
         let css = this.iframe.contentDocument.styleSheets[0];
-        for (let i: number = 0; i < css.cssRules.length; i++) {
-            let estaRegra = (css.cssRules[i] as any);
-            if (estaRegra.selectorText && estaRegra.selectorText.toString().toLowerCase() == "body::before") {
-                css.removeRule(i);
-                break;
+        if (css != undefined)
+            for (let i: number = 0; i < css.cssRules.length; i++) {
+                let estaRegra = (css.cssRules[i] as any);
+                if (estaRegra.selectorText && estaRegra.selectorText.toString().toLowerCase() == "body::before") {
+                    css.removeRule(i);
+                    break;
+                }
             }
-        }
     }
+
+
     public urlIframe: string;
     public seletorIframe: string;
     //este somente fica válido depois que o documento termina de carregar
     public iframe: HTMLIFrameElement;
     public iframeJquery: JQuery<HTMLIFrameElement>;
     public urlBase: string;
-
 }
