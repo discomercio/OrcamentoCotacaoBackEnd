78aba31c2fa3a1a0acf2118af2b0dffcfb190e27
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2021-02-12 11:35:39 -0300

    Implementações de métodos em PedidoFaltandoImplementarSteps e teste indicador.feature a ser reativado na Loja

--- Global/Especificacao/Especificacao/Pedido/PedidoFaltandoImplementarSteps.cs
+++ Global/Especificacao/Especificacao/Pedido/PedidoFaltandoImplementarSteps.cs
@@ -15,48 +15,114 @@ namespace Especificacao.Especificacao.Pedido
         [Given(@"fazer esta validação, no pedido e prepedido")]
         public void GivenFazerEstaValidacaoNoPedidoEPrepedido()
         {
-            
+
+        }
+        [Given(@"Pedido base")]
+        public void GivenPedidoBase()
+        {
+
         }
         [Given(@"Pedido base com endereco de entrega")]
         public void GivenPedidoBaseComEnderecoDeEntrega()
         {
-            
+
         }
 
         [When(@"Informo ""(.*)"" = ""(.*)""")]
         public void WhenInformo(string p0, string p1)
         {
-            
+
         }
 
         [Then(@"Sem nehum erro")]
         public void ThenSemNehumErro()
         {
-            
+
         }
 
         [Then(@"Erro ""(.*)""")]
         public void ThenErro(string p0)
         {
-            
+
         }
 
         [Given(@"Pedido base cliente PJ com endereço de entrega PF")]
         public void GivenPedidoBaseClientePJComEnderecoDeEntregaPF()
         {
-            
+
         }
 
         [Then(@"Sem nenhum erro")]
         public void ThenSemNenhumErro()
         {
-            
+
         }
 
         [Given(@"Pedido base cliente PJ com endereço de entrega PJ")]
         public void GivenPedidoBaseClientePJComEnderecoDeEntregaPJ()
         {
+
+        }
+
+        [Given(@"Ignorar cenário no ambiente ""(.*)""")]
+        public void GivenIgnorarCenarioNoAmbiente(string p0)
+        {
+
+        }
+
+        [Given(@"Reiniciar appsettings")]
+        public void GivenReiniciarAppsettings()
+        {
             
         }
+
+        [Given(@"Reiniciar banco ao terminar cenário")]
+        public void GivenReiniciarBancoAoTerminarCenario()
+        {
+
+        }
+
+        [Given(@"Limpar tabela ""(.*)""")]
+        public void GivenLimparTabela(string tabela)
+        {
+            
+        }
+
+        [Given(@"Novo registro na tabela ""(.*)""")]
+        public void GivenNovoRegistroNaTabela(string p0)
+        {
+            
+        }
+
+        [Given(@"Novo registro em ""(.*)"", campo ""(.*)"" = ""(.*)""")]
+        public void GivenNovoRegistroEmCampo(string tabela, string p0, string p1)
+        {
+            
+        }
+
+        [Given(@"Gravar registro em ""(.*)""")]
+        public void GivenGravarRegistroEm(string p0)
+        {
+            
+        }
+
+        [Given(@"Reiniciar permissões do usuário quando terminar o cenário")]
+        public void GivenReiniciarPermissoesDoUsuarioQuandoTerminarOCenario()
+        {
+
+        }
+
+        [Given(@"Usuário com permissão ""(.*)""")]
+        public void GivenUsuarioComPermissao(string p0)
+        {
+            
+        }
+
+        [Given(@"Usuário sem permissão ""(.*)""")]
+        public void GivenUsuarioSemPermissao(string p0)
+        {
+            
+        }
+
     }
 }
