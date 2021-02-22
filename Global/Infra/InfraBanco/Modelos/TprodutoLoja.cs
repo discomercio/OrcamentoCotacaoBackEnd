6a92733e0edf3dbfe6d3314fcb8cd81f3e8699ad
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2021-02-22 13:55:24 -0300

    merge

--- Global/Infra/InfraBanco/Modelos/TprodutoLoja.cs
+++ Global/Infra/InfraBanco/Modelos/TprodutoLoja.cs
@@ -48,7 +48,7 @@ namespace InfraBanco.Modelos
         public short? Qtde_Max_Venda { get; set; }
 
         [Column("excluido_status")]
-        public short Excluido_status { get; set; }
+        public short? Excluido_status { get; set; }
 
 #if RELEASE_BANCO_PEDIDO || DEBUG_BANCO_DEBUG
         public Tfabricante Tfabricante { get; set; }
