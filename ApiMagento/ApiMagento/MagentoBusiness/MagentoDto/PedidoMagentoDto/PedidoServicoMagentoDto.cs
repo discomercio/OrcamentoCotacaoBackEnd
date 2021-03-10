717e4c4ad47f8666298f0b46f61236b2f2899c9e
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2021-03-10 18:37:24 -0300

    swagger do PedidoServicoMagentoDto

--- ApiMagento/ApiMagento/MagentoBusiness/MagentoDto/PedidoMagentoDto/PedidoServicoMagentoDto.cs
+++ ApiMagento/ApiMagento/MagentoBusiness/MagentoDto/PedidoMagentoDto/PedidoServicoMagentoDto.cs
@@ -8,26 +8,41 @@ namespace MagentoBusiness.MagentoDto.PedidoMagentoDto
     public class PedidoServicoMagentoDto
     {
 #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
+        /// <summary>
+        /// Campo sku
+        /// <hr />
+        /// </summary>
         [Required]
         [MaxLength(8)]
-        public string Sku_produto { get; set; } //  = NumProduto
+        public string Sku_produto { get; set; } //Produto
 
+        /// <summary>
+        /// Campo name
+        /// <hr />
+        /// </summary>
         [Required]
         [MaxLength(120)]
         public string Descricao { get; set; }
 
+        /// <summary>
+        /// Campo qty_ordered
+        /// <hr />
+        /// </summary>
         [Required]
         public short Qtde { get; set; }
 
         /// <summary>
-        /// Preco_bruto: preço de venda do item, gravado em preco_lista
+        /// Preco_bruto: preço de venda do item. Campo original_price.
+        /// <br />Comentário interno: gravado em t_PEDIDO_ITEM_SERVICO.preco_lista
         /// <hr />
         /// </summary>
         [Required]
         public decimal Preco_bruto { get; set; }
 
         /// <summary>
-        /// Preco_liquido: preço que seria impresso na nota fiscal, gravado em preco_venda e preco_nf 
+        /// Preco_liquido: preço que seria impresso na nota fiscal. Campo row_total / campo qty_ordered.
+        /// <br />Comentário interno: gravado em t_PEDIDO_ITEM_SERVICO.preco_venda e preco_nf 
         /// <hr />
         /// </summary>
         [Required]
