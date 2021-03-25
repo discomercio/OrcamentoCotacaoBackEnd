c8f58c67a8b84543b70103b8e50bd60cccce82b8
Eduardo Alvarez Perez <Eduardo.Perez@itssolucoes.com.br>
2021-03-25 16:13:02 -0300

    ajustes testes

--- Global/Especificacao/Especificacao/Pedido/PedidosSimultaneosSteps.cs
+++ Global/Especificacao/Especificacao/Pedido/PedidosSimultaneosSteps.cs
@@ -22,6 +22,8 @@ namespace Especificacao.Especificacao.Pedido
         [Given(@"Testar pedidos simultâneos")]
         public void GivenTestarPedidosSimultaneos()
         {
+            PrepararBancoDados();
+
             //conta o número de pedidos
             var pedidos = (from p in contextoBdProvider.GetContextoLeitura().Tpedidos select p).Count();
             var multiplicadorPorPedido = 2; //magento e loja
@@ -37,6 +39,37 @@ namespace Especificacao.Especificacao.Pedido
             //verifica o total de pedidos criados
             var novosPedidos = (from p in contextoBdProvider.GetContextoLeitura().Tpedidos select p).Count();
             Assert.Equal(pedidos + numeroThreads * pedidosPorThread * multiplicadorPorPedido, novosPedidos);
+        }
+
+        private static void PrepararBancoDados()
+        {
+            var servicos = Testes.Utils.InjecaoDependencia.ProvedorServicos.ObterServicos();
+            //precisa restaurar o banco
+            var bd = new Testes.Utils.BancoTestes.InicializarBancoGeral(servicos.GetRequiredService<InfraBanco.ContextoBdProvider>(), servicos.GetRequiredService<InfraBanco.ContextoCepProvider>());
+            bd.InicializarForcado();
+
+            //criamos uma vez o pedido na mesma thread.
+            //se não fizermos isso, temos erros de timeout no acesso ao banco por causa do multithread
+            //porque o nosso gerenciador de serviços não é thread-safe
+            //de qualquer forma, aqui não queremos testar o multithread do ambiente de testes, e sim
+            //o multithread da criação do pedido
+            {
+                PedidoSteps pedidoSteps = new PedidoSteps();
+                pedidoSteps.GivenIgnorarCenarioNoAmbiente("Especificacao.Prepedido.PrepedidoSteps");
+                try
+                {
+                    //dorme um tempo aleatório apra deslocar as threads
+                    Thread.Sleep(new Random().Next(1, 500));
+                    Testes.Utils.LogTestes.LogTestes.LogMensagemOperacao($"CriarThreads iniciar pedido thread {Thread.CurrentThread.ManagedThreadId} na thread principal", typeof(PedidosSimultaneosSteps));
+                    pedidoSteps.GivenPedidoBase();
+                    pedidoSteps.ThenSemNenhumErro();
+                }
+                catch (Exception e)
+                {
+                    Testes.Utils.LogTestes.LogTestes.ErroNosTestes($"EXCECAO: ERRO: na criacao do pedido: {e.Message} {e.StackTrace} {e.ToString()}");
+                    throw;
+                }
+            }
         }
 
         private static void CriarThreads(int pedidosPorThread, int numeroThreads, List<Thread> threads)
