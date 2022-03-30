939bd644b0dca9a3f86972b76a44267cb2f04d7d
Kleber Lunguinho <kleber.lunguinho@itssolucoes.com.br>
2022-03-30 00:43:10 -0300

    Incluído infra e modelo para Propriedades do Produto do Catálogo.

---
+++ Global/Infra/InfraBanco/Modelos/TProdutoCatalogoPropriedade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace InfraBanco.Modelos
+{
+    [Table("t_PRODUTO_CATALOGO_PROPRIEDADE")]
+    public class TProdutoCatalogoPropriedade
+    {       
+        [Key]
+        [Column("Id")]
+        public int id { get; set; }
+        [Required]
+        public short IdCfgTipoPropriedade { get; set; }
+        [Required]
+        public short IdCfgTipoPermissaoEdicaoCadastro { get; set; }
+        [Required]
+        public short IdCfgDataType { get; set; }
+        [Required]
+        public string descricao { get; set; }
+        [Required]
+        public bool oculto { get; set; }
+        [Required]
+        public int ordem { get; set; }
+        [Required]
+        public DateTime dt_cadastro { get; set; }
+        [Required]
+        public string usuario_cadastro { get; set; }
+    }
+}
---
+++ Global/Produto/Dados/ProdutoCatalogoPropriedadeDados.cs
@@ -0,0 +1,38 @@
+﻿using InfraBanco.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Produto.Dados
+{
+    public class ProdutoCatalogoPropriedadeDados
+    {
+        public int id { get; set; }
+        [Required]
+        public short IdCfgTipoPropriedade { get; set; }
+        [Required]
+        public short IdCfgTipoPermissaoEdicaoCadastro { get; set; }
+        [Required]
+        public short IdCfgDataType { get; set; }
+        [Required]
+        public string descricao { get; set; }
+        [Required]
+        public bool oculto { get; set; }
+        [Required]
+        public int ordem { get; set; }
+        [Required]
+        public DateTime dt_cadastro { get; set; }
+        [Required]
+        public string usuario_cadastro { get; set; }
+
+    }
+}
+        
+        
+
+    
+
+    
+
+    
