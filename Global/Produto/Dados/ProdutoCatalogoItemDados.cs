6e118b276ad4865c66edf6cedfdb5e0da781456e
Kleber Lunguinho <kleber.lunguinho@itssolucoes.com.br>
2022-04-25 07:42:31 -0300

    Add class de modelo de retorno dos itens do catálogo.

---
+++ Global/Produto/Dados/ProdutoCatalogoItemDados.cs
@@ -0,0 +1,24 @@
+﻿using InfraBanco.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Produto.Dados
+{
+    public class ProdutoCatalogoItemDados
+    {
+        public int idProdutoCatalogoPropriedade { get; set; }
+        public string nome { get; set; }
+        public string valor_item { get; set; }
+        public string valor_opcao { get; set; }
+    }
+}
+        
+        
+
+    
+
+    
+
+    
