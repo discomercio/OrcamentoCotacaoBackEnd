609488b995012d981ef2b5b8b7f9fef5bf5f398b
Kleber Lunguinho <kleber.lunguinho@itssolucoes.com.br>
2022-04-27 19:41:12 -0300

    Incluído modelo de retorno para fabricantes.

---
+++ Global/Produto/Dados/FabricanteDados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produto.Dados
+{
+    public class FabricanteDados
+    {
+        public string Fabricante { get; set; }
+        public string Nome { get; set; }
+    }
+}
