46872d0e1b0c3f5c288d5b0411dec4c71a9b398f
Mauro Regis <mauro.lima>
2022-06-21 09:54:24 -0300

    #60 - Tema por loja

---
+++ Global/Loja/Dto/LojaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Loja.Dto
+{
+    public class LojaViewModel
+    {
+        [JsonProperty("loja")]
+        public string Loja { get; set; }
+
+        [JsonProperty("imagemLogotipo")]
+        public string ImagemLogotipo { get; set; }
+
+        [JsonProperty("corCabecalho")]
+        public string CorCabecalho { get; set; }
+    }
+}
--- OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Bll/LojaOrcamentoCotacaoBll.cs
+++ OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Bll/LojaOrcamentoCotacaoBll.cs
@@ -2,7 +2,10 @@
 using InfraBanco.Modelos;
 using InfraBanco.Modelos.Filtros;
 using Loja;
+using Loja.Dto;
+using OrcamentoCotacaoBusiness.Models.Response;
 using OrcamentoCotacaoBusiness.Models.Response.FormaPagamento;
+using System;
 using System.Collections.Generic;
 
 namespace OrcamentoCotacaoBusiness.Bll
@@ -25,16 +28,24 @@ namespace OrcamentoCotacaoBusiness.Bll
         {
             var retorno = _lojaBll.BuscarPercMaxPorLoja(loja);
 
-            return new PercMaxDescEComissaoResponseViewModel
+            if (retorno != null)
             {
-                PercMaxComissao = retorno.PercMaxComissao,
-                PercMaxComissaoEDesconto = retorno.PercMaxComissaoEDesconto,
-                PercMaxComissaoEDescontoNivel2 = retorno.PercMaxComissaoEDescontoNivel2,
-                PercMaxComissaoEDescontoNivel2PJ = retorno.PercMaxComissaoEDescontoNivel2PJ,
-                PercMaxComissaoEDescontoPJ = retorno.PercMaxComissaoEDescontoPJ,
-            };
+                return new PercMaxDescEComissaoResponseViewModel
+                {
+                    PercMaxComissao = retorno.PercMaxComissao,
+                    PercMaxComissaoEDesconto = retorno.PercMaxComissaoEDesconto,
+                    PercMaxComissaoEDescontoNivel2 = retorno.PercMaxComissaoEDescontoNivel2,
+                    PercMaxComissaoEDescontoNivel2PJ = retorno.PercMaxComissaoEDescontoNivel2PJ,
+                    PercMaxComissaoEDescontoPJ = retorno.PercMaxComissaoEDescontoPJ,
+                };
+            }
 
             return null;
+        }
+
+        public LojaViewModel BuscarLojaEstilo(string loja)
+        {
+            return _lojaBll.BuscarLojaEstilo(loja);
         }
     }
 }
