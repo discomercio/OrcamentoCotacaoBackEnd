0dba0f488acd2a3c0155dcefeea8d343b26f5c98
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-09-02 13:32:16 -0300

    Adicionando filtro t_orcamento_email - #185

---
+++ Global/Infra/InfraBanco/Modelos/Filtros/TorcamentoCotacaoEmailFiltro.cs
@@ -0,0 +1,13 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfraBanco.Modelos.Filtros
+{
+    public class TorcamentoCotacaoEmailFiltro : IFilter
+    {
+        public int? IdOrcamentoCotacao { get; set; }
+        public long? IdOrcamentoCotacaoEmailQueue { get; set; }
+    }
+}
