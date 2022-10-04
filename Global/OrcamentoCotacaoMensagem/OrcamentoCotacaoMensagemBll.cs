9071ac78cfb2e0828ac50d6ab49134c5b3918bec
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-10-04 20:11:54 -0300

    Ajustes complementares 9e17a4cf

--- Global/OrcamentoCotacaoMensagem/OrcamentoCotacaoMensagemBll.cs
+++ Global/OrcamentoCotacaoMensagem/OrcamentoCotacaoMensagemBll.cs
@@ -53,31 +53,7 @@ namespace OrcamentoCotacaoMensagem
                 orcamentoCotacaoMensagemStatus.Lida = false;
                 orcamentoCotacaoMensagemStatus.PendenciaTratada = false;
 
-                var torcamentoCotacaoMensagemStatus = _orcamentoCotacaoMensagemStatusBll.InserirComTransacao(orcamentoCotacaoMensagemStatus, contextoBdGravacao);
-
-                TorcamentoCotacaoMensagem participantes = null;
-
-                if (orcamentoCotacaoMensagem.IdUsuarioRemetente == 0)
-                {
-                    participantes = (from ocm in contextoBdGravacao.TorcamentoCotacaoMensagem
-                                                   where ocm.IdOrcamentoCotacao == orcamentoCotacaoMensagem.IdOrcamentoCotacao &&
-                                                   ocm.IdUsuarioRemetente != 0 && 
-                                                   ocm.IdUsuarioRemetente != orcamentoCotacaoMensagem.IdUsuarioDestinatario
-                                        select ocm).FirstOrDefault();                
-;
-
-                    if (participantes != null)
-                    {
-
-                        orcamentoCotacaoMensagemStatus.IdOrcamentoCotacaoMensagem = torcamentoCotacaoMensagem.Id;
-                        orcamentoCotacaoMensagemStatus.IdTipoUsuarioContexto = participantes.IdTipoUsuarioContextoRemetente;
-                        orcamentoCotacaoMensagemStatus.IdUsuario = participantes.IdUsuarioRemetente;
-                        orcamentoCotacaoMensagemStatus.Lida = false;
-                        orcamentoCotacaoMensagemStatus.PendenciaTratada = false;
-                        torcamentoCotacaoMensagemStatus = _orcamentoCotacaoMensagemStatusBll.InserirComTransacao(orcamentoCotacaoMensagemStatus, contextoBdGravacao);
-                    }
-                    
-                }
+                var torcamentoCotacaoMensagemStatus = _orcamentoCotacaoMensagemStatusBll.InserirComTransacao(orcamentoCotacaoMensagemStatus, contextoBdGravacao);      
 
                 if (torcamentoCotacaoMensagemStatus != null)
                 {
