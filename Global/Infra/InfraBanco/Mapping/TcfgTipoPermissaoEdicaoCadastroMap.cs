6761d0e75138e5ba804696e68bd02e62385a8b0f
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2022-10-17 12:14:49 -0300

    Crição de tabela TcfgTipoPermissaoEdicaoCadastro e TcfgTipoPermissaoEdicaoCadastroMap

---
+++ Global/Infra/InfraBanco/Mapping/TcfgTipoPermissaoEdicaoCadastroMap.cs
@@ -0,0 +1,33 @@
+﻿using InfraBanco.Modelos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfraBanco.Mapping
+{
+    public class TcfgTipoPermissaoEdicaoCadastroMap : IEntityTypeConfiguration<TcfgTipoPermissaoEdicaoCadastro>
+    {
+        public void Configure(EntityTypeBuilder<TcfgTipoPermissaoEdicaoCadastro> builder)
+        {
+            builder.ToTable("t_CFG_TIPO_PERMISSAO_EDICAO_CADASTRO");
+            builder.HasKey(o => o.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("Id")
+                .HasColumnType("smallint")
+                .IsRequired();
+
+            builder.Property(x => x.Sigla)
+                .HasColumnName("Sigla")
+                .HasColumnType("varchar(20)")
+                .IsRequired();
+
+            builder.Property(x => x.Descricao)
+                .HasColumnName("Descricao")
+                .HasColumnType("varchar(80)")
+                .IsRequired();
+        }
+    }
+}
---
+++ Global/Infra/InfraBanco/Modelos/TcfgTipoPermissaoEdicaoCadastro.cs
@@ -0,0 +1,14 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfraBanco.Modelos
+{
+    public class TcfgTipoPermissaoEdicaoCadastro : IModel
+    {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Descricao { get; set; }
+    }
+}
