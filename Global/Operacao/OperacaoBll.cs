6347fd8d9da4cbf4083b7933c1c22526a9f4f49f
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-10-24 11:00:19 -0300

    Adicionando projeto Operacao

---
+++ Global/Operacao/OperacaoBll.cs
@@ -0,0 +1,21 @@
+﻿using ClassesBase;
+using InfraBanco;
+using InfraBanco.Modelos;
+using InfraBanco.Modelos.Filtros;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Operacao
+{
+    public class OperacaoBll : BaseBLL<Toperacao, ToperacaoFiltro>
+    {
+
+        private OperacaoData _data { get; set; }
+
+        public OperacaoBll(ContextoBdProvider contextoBdProvider) : base(new OperacaoData(contextoBdProvider))
+        {
+            _data = new OperacaoData(contextoBdProvider);
+        }
+    }
+}
---
+++ Global/Operacao/OperacaoData.cs
@@ -0,0 +1,63 @@
+﻿using ClassesBase;
+using InfraBanco;
+using InfraBanco.Modelos;
+using InfraBanco.Modelos.Filtros;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Operacao
+{
+    public class OperacaoData : BaseData<Toperacao, ToperacaoFiltro>
+    {
+        private readonly ContextoBdProvider contextoProvider;
+
+        public OperacaoData(ContextoBdProvider contextoProvider)
+        {
+            this.contextoProvider = contextoProvider;
+        }
+
+        public Toperacao Atualizar(Toperacao obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Toperacao AtualizarComTransacao(Toperacao model, ContextoBdGravacao contextoBdGravacao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Excluir(Toperacao obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirComTransacao(Toperacao obj, ContextoBdGravacao contextoBdGravacao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Toperacao Inserir(Toperacao obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Toperacao InserirComTransacao(Toperacao model, ContextoBdGravacao contextoBdGravacao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Toperacao> PorFilroComTransacao(ToperacaoFiltro obj, ContextoBdGravacao contextoBdGravacao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Toperacao> PorFiltro(ToperacaoFiltro obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
