2e1e5dc8a96a33fcdf4d4c15e1449ea43635b198
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-11-16 18:14:13 -0300

    Incluindo classes de Response e Request de expiração de senha

---
+++ OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Models/Response/ExpiracaoSenhaResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace OrcamentoCotacaoBusiness.Models.Response
+{
+    public sealed class ExpiracaoSenhaResponseViewModel : IViewModelResponse
+    {
+        public ExpiracaoSenhaResponseViewModel(bool sucesso, string mensagemRetorno)
+        {
+            Sucesso = sucesso;
+            MensagemRetorno = mensagemRetorno;
+        }
+
+        public bool Sucesso { get; private set; }
+        public string MensagemRetorno { get; private set; }
+    }
+}
