7b16b747250ff13939ae2a0e367ae9fe13d3852b
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-11-23 00:39:58 -0300

    Incluindo filtro por nome do vendedor - #361

--- Global/Infra/InfraBanco/Modelos/Filtros/TorcamentistaEindicadorFiltro.cs
+++ Global/Infra/InfraBanco/Modelos/Filtros/TorcamentistaEindicadorFiltro.cs
@@ -8,6 +8,7 @@ namespace InfraBanco.Modelos.Filtros
         public string apelido { get; set; }
         public string datastamp { get; set; }
         public string vendedorId { get; set; }
+        public string vendedor { get; set; }
         public string loja { get; set; }
         public int acessoHabilitado { get; set; }
         public string status { get; set; }
