97d45584b4368b03156f8f88e23c3baea01f677f
Kleber <kleber.lunguinho@itssolucoes.com.br>
2022-12-05 22:33:45 -0300

    Incluindo objeto de request para o dashboard

---
+++ OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Models/Request/Dashboard/DashboardRequest.cs
@@ -0,0 +1,13 @@
+﻿using UtilsGlobais.RequestResponse;
+
+namespace OrcamentoCotacaoBusiness.Models.Request.Dashboard
+{
+    public sealed class DashboardRequest : RequestBase
+    {
+        public string Loja { get; set; }
+
+        public int Status = 1;
+
+        public string Origem = "DASHBOARD";
+    }
+}
