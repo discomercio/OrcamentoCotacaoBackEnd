901fe4d17321c2b5cd8b5e3b06dd8521dddbe6b5
Kleber <kleber.lunguinho@itssolucoes.com.br>
2023-03-30 15:00:11 -0300

    Ajustes no retorno da versão da api Sistema para retornar a versão da DLL

--- OrcamentoCotacaoApi/OrcamentoCotacaoApi/Controllers/SistemaController.cs
+++ OrcamentoCotacaoApi/OrcamentoCotacaoApi/Controllers/SistemaController.cs
@@ -8,38 +8,27 @@ using System.Threading.Tasks;
 using OrcamentoCotacaoApi.Utils;
 using System.Text.Json;
 using Microsoft.AspNetCore.Http;
-
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.AspNetCore.Hosting;
 
 namespace OrcamentoCotacaoApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Authorize]
+    
     public class SistemaController : BaseController
     {
         private readonly ILogger<SistemaController> _logger;
+        private readonly IHostingEnvironment _env;
 
-        public SistemaController(ILogger<SistemaController> logger)
+        public SistemaController(ILogger<SistemaController> logger, IHostingEnvironment env)
         {
             _logger = logger;
+            _env = env;
         }
 
-        [HttpGet("cache")]
-        public async Task<IActionResult> RetornarVersaoCache()
-        {
-            _logger.LogInformation("RetornarVersaoCache");
-
-            try
-            {               
-                var versaoApi = JsonSerializer.Serialize(HttpContext.Session.GetString("versaoApi"));
-
-                return Ok(versaoApi);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
 
         [HttpGet("versao")]
         public async Task<IActionResult> RetornarVersao()
@@ -48,9 +37,30 @@ namespace OrcamentoCotacaoApi.Controllers
 
             try
             {
-                var versaoApi = JsonSerializer.Serialize(System.IO.File.ReadAllText("version.txt"));
 
-                return Ok(versaoApi);
+                Process currentProcess = Process.GetCurrentProcess();
+
+                string assemblyPath = string.Empty;
+
+                FileVersionInfo versionInfo;
+
+                if (_env.IsDevelopment())
+                {
+                    assemblyPath = "bin/Debug/net7.0/OrcamentoCotacaoApi.dll";
+                }
+                else
+                {
+                    assemblyPath = "OrcamentoCotacaoApi.dll";
+                }
+                    
+                versionInfo = FileVersionInfo.GetVersionInfo(assemblyPath);               
+                
+                return new ContentResult 
+                { 
+                    Content = versionInfo.FileVersion.ToString(), 
+                    ContentType= "text/plain",
+                    StatusCode= 200
+                };
             }
             catch (Exception ex)
             {
