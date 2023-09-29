cf1bfa4a9fa2a61198c62cb68a1e95e21ffaf019
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2023-09-29 14:31:38 -0300

    Criação de projeto para relatórios, remoção de utilização de contexto com bd sem utilidade

---
+++ Global/Relatorios/RelatoriosBll.cs
@@ -0,0 +1,21 @@
+﻿using InfraBanco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relatorios
+{
+    public class RelatoriosBll
+    {
+        private RelatoriosData _data { get; set; }
+
+        public RelatoriosBll(ContextoRelatorioProvider contextoProvider)
+        {
+            _data = new RelatoriosData(contextoProvider);
+        }
+
+        
+    }
+}
---
+++ Global/Relatorios/RelatoriosData.cs
@@ -0,0 +1,21 @@
+﻿using InfraBanco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relatorios
+{
+    public class RelatoriosData
+    {
+        private readonly ContextoRelatorioProvider contexto;
+
+        public RelatoriosData(ContextoRelatorioProvider _contexto)
+        {
+            contexto = _contexto;
+        }
+
+
+    }
+}
