04a29ed0d27fc3b10d83aca11c5dd76770e1fc6e
Gabriel Prada Teodoro <Gabriel.Teodoro@itssolucoes.com.br>
2023-10-11 14:06:04 -0300

    Ajuste de classe de request, ajuste na hora de data de criação e validade, ajuste na validação de datas de início e fim da criação do orçamento para relatório de Dados de orçamento

--- OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Bll/RelatoriosBll.cs
+++ OrcamentoCotacaoApi/OrcamentoCotacaoBusiness/Bll/RelatoriosBll.cs
@@ -129,7 +129,7 @@ namespace OrcamentoCotacaoBusiness.Bll
                 response.Mensagem = "O campo 'Fim da criação' é obrigatório!";
                 return response;
             }
-            var diferenca = (request.DtFim.Value - request.DtInicio.Value).TotalDays;
+            var diferenca = (request.DtFim.Value.Date - request.DtInicio.Value.Date).TotalDays;
             if (diferenca > config.MaxPeriodoConsulta_RelatorioGerencial)
             {
                 response.Mensagem = $"A diferença entre as datas de \"Início da criação\" e \"Fim da criação\" ultrapassa o período de {config.MaxPeriodoConsulta_RelatorioGerencial} de dias!";
@@ -196,8 +196,8 @@ namespace OrcamentoCotacaoBusiness.Bll
                     QtdeParcelasFormaOpcaoAprovada= item.QtdeParcelasFormaOpcaoAprovada,
                     StatusDescSuperiorOpcaoAprovada = item.StatusDescSuperiorOpcaoAprovada,
                     DescMedioDescSuperiorOpcaoAprovada = !string.IsNullOrEmpty(item.DescMedioDescSuperiorOpcaoAprovada)? Math.Round(decimal.Parse(item.DescMedioDescSuperiorOpcaoAprovada), 2, MidpointRounding.AwayFromZero): null,
-                    DataCadastro = DateTime.TryParse(item.Criacao, out var datacria) ? datacria : null,
-                    Validade = DateTime.TryParse(item.Expiracao, out var exp) ? exp : null,
+                    DataCadastro = DateTime.TryParse(item.Criacao, out var datacria) ? datacria.AddHours(9) : null,
+                    Validade = DateTime.TryParse(item.Expiracao, out var exp) ? exp.AddHours(9) : null,
                 };
                 response.ListaDadosOrcamento.Add(dadoOrcamento);
             }
